25503161351993b414f797f76147f9c4329a1e33
Lucas Duarte <lucasduartedev@gmail.com>
2020-03-22 12:15:21 -0300

    Adicionando comentários ao código

--- Tela.py
+++ Tela.py
@@ -1,5 +1,7 @@
+# Biblioteca para definição da interface gráfica
 import PySimpleGUI as sg
 
+# Função para retornar os Getters e Setters
 def gerador(palavra):
     print('@property')
     print('def {}(self):'.format(palavra))
@@ -11,14 +13,15 @@ def gerador(palavra):
 
 # Definição do layout
 layout = [
-    [
+    [   # Campo de texto para recebe a PALAVRA
         sg.Input(key='palavra', size=(15, 0)),
+        # Botão para acionar a função 'geradora'
         sg.Button('GERAR', key='btn_gerar', size=(10, 0))
     ],
-    [
-        sg.Output(size=(30, 20))
+    [   # Campo de texto que mosta o resultado
+        sg.Output(size=(30, 10))
     ],
-    [
+    [   # Botão para chefar a aplicação
         sg.Button('FECHAR', key='btn_fechar', size=(10, 0))
     ]
 ]
@@ -37,9 +40,9 @@ while True:
     # Palavra informada
     palavra = values['palavra']
 
+    # Botão 'gerar' aciona a função 'geradora'
     if event in (None, 'btn_gerar'):
         gerador(palavra)
 
-    
 # Fechar janela
 window.close()
